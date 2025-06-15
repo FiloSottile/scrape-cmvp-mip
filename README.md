9cede5f10c54872aa9b44a010f01d329fb264fc8
Filippo Valsorda <hi@filippo.io>
2025-06-15 10:35:23 +0200

    Move summary to README

--- README.md
+++
@@ -1,3 +0,0 @@
-# Scheduled scraper
-
-For https://csrc.nist.gov/Projects/cryptographic-module-validation-program/modules-in-process/modules-in-process-list
--- scrape.sh
+++ scrape.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ./download.sh 'https://csrc.nist.gov/Projects/cryptographic-module-validation-program/modules-in-process/modules-in-process-list'
-go run filippo.io/mostly-harmless/mip@main csrc.nist.gov-Projects-cryptographic-module-validation-program-modules-in-process-modules-in-process-list.html | tee summary.txt
+go run filippo.io/mostly-harmless/mip@main csrc.nist.gov-Projects-cryptographic-module-validation-program-modules-in-process-modules-in-process-list.html | tee README
